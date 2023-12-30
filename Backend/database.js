cc225b50ec2e35163f48915549c1d4e619f70cd3
Kratika Rathore <116361926+Kratirathore@users.noreply.github.com>
2023-12-30 11:24:31 +0530

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const mongooseURI='mongodb://127.0.0.1:27017/Login'
 const connecttomongo=()=>{
-    mongoose.connect(mongooseURI,console.log("You have been Successfully Connected with DoorPoint at MongoDB"))
+    mongoose.connect(mongooseURI,console.log("You have been Successfully Connected with Demo at MongoDB"))
 
 }
 module.exports=connecttomongo;
